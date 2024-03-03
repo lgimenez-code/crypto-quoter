e8bf97fe559e456edb46181012fde5a64eebb01b
lgimenez-code <lucasgimenez2007@gmail.com>
2024-03-03 10:44:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import styled from "@emotion/styled";
+import ImgCrypto from './img/img-crypto.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Lto', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-wight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Image src={ImgCrypto} alt="Image Crypto"/>
+      <Heading>Desde la APP</Heading>
+    </Container>
+  )
+}
+
+export default App;
